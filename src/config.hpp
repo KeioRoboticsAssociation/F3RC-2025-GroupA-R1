6ee1daab08783947cb69944ced796b752bce4521
Toyoda05 <d_toyoda@keio.jp>
2025-08-10 14:45:31 +0900

    fix: fix pin num

--- src/config.hpp
+++ src/config.hpp
@@ -5,24 +5,23 @@
 
 namespace DigitalOutPins
 {
-    constexpr PinName OMUNI_MOTOR1_DIR  = PB_10;     // オムニホイールDC1の方向
-    constexpr PinName OMUNI_MOTOR2_DIR  = PC_7 ;     // オムニホイールDC2の方向
-    constexpr PinName OMUNI_MOTOR3_DIR  = PB_4 ;     // オムニホイールDC3の方向
+    constexpr PinName OMUNI_MOTOR1_DIR  = PA_10;     // オムニホイールDC1の方向
+    constexpr PinName OMUNI_MOTOR2_DIR  = PA_0 ;     // オムニホイールDC2の方向
+    constexpr PinName OMUNI_MOTOR3_DIR  = PC_3 ;     // オムニホイールDC3の方向
 
-    constexpr PinName PSEUDO_SERVO1_DIR = PB_9 ;     // 疑似サーボ用DC1の方向
-    constexpr PinName PSEUDO_SERVO1_DIR = PB_6 ;     // 疑似サーボ用DC2の方向
+    constexpr PinName PSEUDO_SERVO1_DIR = PC_2 ;     // 疑似サーボ用DC1の方向
 }
 
 
 
 namespace PwmOutPins
 {
-    constexpr PinName OMUNI_MOTOR1_PWM  = PA_8 ;     // オムニホイールDC1のPWM (Tim1)
-    constexpr PinName OMUNI_MOTOR2_PWM  = PA_9 ;     // オムニホイールDC2のPWM (Tim1)
-    constexpr PinName OMUNI_MOTOR3_PWM  = PA_10;     // オムニホイールDC3のPWM (Tim1)
+    constexpr PinName OMUNI_MOTOR1_PWM  = PA_15;     // オムニホイールDC1のPWM (Tim1)
+    constexpr PinName OMUNI_MOTOR2_PWM  = PB_7 ;     // オムニホイールDC2のPWM (Tim1)
+    constexpr PinName OMUNI_MOTOR3_PWM  = PA_1 ;     // オムニホイールDC3のPWM (Tim1)
 
-    constexpr PinName PSEUDO_SERVO1_PWM = PA_6 ;     // 疑似サーボ用DC1のPWM (Tim3)
-    constexpr PinName PSEUDO_SERVO1_PWM = PA_7 ;     // 疑似サーボ用DC2のPWM (Tim3)
+    constexpr PinName PSEUDO_SERVO1_PWM = PB_0 ;     // 疑似サーボ用DC1のPWM (Tim3)
 
-    constexpr PinName SERVO_PWM         = PA_15;     // サーボ用PWM (Tim2)
+    constexpr PinName SERVO1_PWM        = PB_2 ;     // サーボ用PWM (Tim2)
+    constexpr PinName SERVO2_PWM        = PA_0 ;     // サーボ用PWM (Tim2)
 }
