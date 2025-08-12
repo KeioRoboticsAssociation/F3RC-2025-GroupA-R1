9dfdf81e3d8638a553bfa0bbe4e67183db9bfe61
5C6F2F <5c6f2f@gmail.com>
2025-08-12 19:09:21 +0900

    feat: 操作を実装

---
+++ src/main.cpp
@@ -0,0 +1,70 @@
+#include <mbed.h>
+#include <stdio.h>
+#include "controller/Controller.hpp"
+#include "components/drivetrain/DriveTrain.hpp"
+
+asm(".global _printf_float"); // printfでfloatを使えるようにする
+
+int main()
+{
+    DCMotor dc1(PwmOutPins::OMUNI_MOTOR1_PWM, DigitalOutPins::OMUNI_MOTOR1_DIR);
+    DCMotor dc2(PwmOutPins::OMUNI_MOTOR2_PWM, DigitalOutPins::OMUNI_MOTOR2_DIR);
+    DCMotor dc3(PwmOutPins::OMUNI_MOTOR3_PWM, DigitalOutPins::OMUNI_MOTOR3_DIR);
+
+    std::array<WheelConfig, 3> config = {
+        WheelConfig{
+            .wheel_radius = DriveConfig::WHEEL_RAD, // ホイール半径
+            .wheel_x = 0.0,                         // ホイールのx座標
+            .wheel_y = DriveConfig::TREAD_RAD,      // ホイールのy座標
+            .wheel_theta = M_PI                     // ホイールの向き
+        },
+        WheelConfig{
+            .wheel_radius = DriveConfig::WHEEL_RAD,
+            .wheel_x = -(float)M_SQRT3 / 2 * DriveConfig::TREAD_RAD,
+            .wheel_y = -(float)0.5 * DriveConfig::TREAD_RAD,
+            .wheel_theta = 5 * M_PI / 3},
+        WheelConfig{
+            .wheel_radius = DriveConfig::WHEEL_RAD,
+            .wheel_x = +(float)M_SQRT3 / 2 * DriveConfig::TREAD_RAD,
+            .wheel_y = -(float)0.5 * DriveConfig::TREAD_RAD,
+            .wheel_theta = M_PI / 3}};
+
+    WheelController<3> wheel_controller(config, {&dc1, &dc2, &dc3});
+
+    Controller joy(PB_10, PC_5, ControllerConfig::BAUD_RATE);
+    DriveTrain<3> drive_train(wheel_controller);
+
+    while (1)
+    {
+        // printf("%5.2f %5.2f %5.2f %5.2f %d %d %d %d %d %d %d %d %d %d %d %d\n",
+        //        joy.rStickX(),   // 左スティックX(左-1.0~1.0右)
+        //        joy.rStickY(),   // 左スティックY(上-1.0~1.0下)
+        //        joy.lStickX(),   // 右スティックX(左-1.0~1.0右)
+        //        joy.lStickY(),   // 右スティックY(上-1.0~1.0下)
+        //        joy.dpadUp(),    // 十字キー上
+        //        joy.dpadDown(),  // 十字キー下
+        //        joy.dpadRight(), // 十字キー右
+        //        joy.dpadLeft(),  // 十字キー左
+        //        joy.cross(),     // ×
+        //        joy.circle(),    // ○
+        //        joy.square(),    // ⬜︎
+        //        joy.triangle(),  // △
+        //        joy.L1(),        // L1
+        //        joy.R1(),        // R1
+        //        joy.L2(),        // L2
+        //        joy.R2());       // R2
+
+        drive_train.move(joy.rStickX(), joy.rStickY(), joy.L2(), joy.R2());
+
+        if (joy.R1())
+        {
+            drive_train.changeToHighSpeedMode();
+        }
+        else if (joy.L1())
+        {
+            drive_train.changeToLowSpeedMode();
+        }
+
+        wait_us(100000);
+    }
+}
